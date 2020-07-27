dcda9707ca197186e83773f8f48e391a8eecb86d
RileyHYZ <ri.hyzhou@gmail.com>
2020-07-27 14:33:52 -0400

    added primary key column

--- db/setup.sql
+++ db/setup.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE IF NOT EXISTS mydb;
+use mydb;
+
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(80) NOT NULL,
@@ -13,6 +15,7 @@ CREATE TABLE IF NOT EXISTS `job` (
   `job_title` varchar(200) NOT NULL,
   `company` varchar(200) NOT NULL,
   `app_process` varchar(200) NOT NULL,
+  `id` int(11),
   PRIMARY KEY (`job_id`),
   CONSTRAINT fk_job_user_id FOREIGN KEY (`id`) REFERENCES users(`id`)
 );
@@ -22,6 +25,7 @@ CREATE TABLE IF NOT EXISTS `post` (
   `content` longtext NOT NULL,
   `tag` varchar(80),
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` int(11),
   PRIMARY KEY (`post_id`),
   CONSTRAINT fk_post_user_id FOREIGN KEY (`id`) REFERENCES users(`id`)
 );
