2203c87bc65c53800a710d520d3edf653f0574a9
RileyHYZ <ri.hyzhou@gmail.com>
2020-07-27 14:56:06 -0400

    updated container script

--- db/script.sh
+++ db/script.sh
@@ -1,4 +1,4 @@
 # download mysql 8.0 image from docker
-docker pull mysql/mysql-server:8.0
+docker build --tag mysqldb .
 # start a docker container and open port 3306
-docker run --name=jobtracking -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw mysql/mysql-server:8.0
+docker run --name jobtracking -p 3306:3306 -e MYSQL_ROOT_PASSWORD=my-secret-pw mysqldb
