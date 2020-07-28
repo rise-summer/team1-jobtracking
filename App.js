f9edb03ba805520d33ef5990634f0b77282dc268
Sharifa Jesmin <56977309+sharifajesmin@users.noreply.github.com>
2020-07-28 05:00:08 -0700

    For Navigation

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+//import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import Login from './login';
+
+
+//import './App.css';
+import Navigation from "./Navigation/Navigation";
+ 
+
+class App extends React.Component{
+  render() {
+    return(
+      <div>
+      <Navigation />
+      </div>
+
+    );  
+  } 
+}
+export default App;
+
+
+
+
+
+
+/*function App() {
+  return (
+    <BrowserRouter>
+        <div className="App">
+         <Login/>
+          <Navigation/>
+          <Navbar />
+          <Switch>
+            {/*<Route exact path="/" component={LandingPage} />
+            <Route exact path="/startups" component={Startups} />
+            
+            <Route path="*" component={NotFoundPage} />
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
+  );
+}
+
+export default App; */
