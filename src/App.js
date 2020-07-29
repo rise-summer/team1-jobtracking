ec6841b5971f1c9dfa07119000a8f48c087bcd25
sharifajesmin <sjesmin@uci.edu>
2020-07-29 09:43:32 -0700

    Frontend-Sign UP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Login from './login';
+//import { BrowserRouter, Switch, Route } from "react-router-dom";
+//import Login from './login';
 
-function App() {
+
+//import './App.css';
+//import Navigation from "./Navigation/Navigation";
+import SignUp from "./SignUp/SignUp";
+ 
+
+class App extends React.Component{
+  render() {
+    return(
+      <div>
+      {/*<Navigation />*/}
+      <SignUp/>
+      </div>
+
+    );  
+  } 
+}
+export default App;
+
+
+
+
+
+
+/*function App() {
   return (
     <BrowserRouter>
         <div className="App">
-          <Login/>
-          {/*<Navbar />*/}
+         <Login/>
+          <Navigation/>
+          <Navbar />
           <Switch>
             {/*<Route exact path="/" component={LandingPage} />
             <Route exact path="/startups" component={Startups} />
             
-            <Route path="*" component={NotFoundPage} />*/}
+            <Route path="*" component={NotFoundPage} />
           </Switch>
-          {/*<Footer />*/}
+          <Footer />
         </div>
       </BrowserRouter>
   );
 }
 
-export default App;
+export default App; */
