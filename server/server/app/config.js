eb38c276daef6b60a1ef79e1ee3e315958876b18
RileyHYZ <ri.hyzhou@gmail.com>
2020-08-09 16:34:15 -0400

    added firebase config

--- server/server/app/config.js
+++ server/server/app/config.js
@@ -11,4 +11,17 @@ const connection = {
   port: dbport
 };
 
-module.exports = connection;
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXHiG2OLne7miagpCizFwNpLvq-RYTxsc',
+  authDomain: 'jobtracking001.firebaseapp.com',
+  databaseURL: 'https://jobtracking001.firebaseio.com',
+  projectId: 'jobtracking001',
+  storageBucket: 'jobtracking001.appspot.com',
+  messagingSenderId: '201224764748',
+  appId: '1:201224764748:web:03dfd879104d6c509063e9'
+};
+
+module.exports = {
+  connection,
+  firebaseConfig
+};
