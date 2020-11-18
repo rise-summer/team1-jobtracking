158708abeb50bd72f195cb21ad947b9e22888d10
Anirudh Murthy <anirudh.murthy0@gmail.com>
2020-11-17 19:44:47 -0500

    addition of redux to trackr

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,74 +10,81 @@ import {
   AdditionDiv,
   AdditionWrapper,
   AdditionButton,
-  SearchTag
+  SearchTag,
+} from "./style.js";
 
-} from './style.js'
+import { connect } from "react-redux";
 
-import {useSelector} from 'react-redux'
-
+const mapStateToProps = (state) => {
+  return { isLoggedIn: state.isLogged };
+};
 
 class Navigation extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: '',
-      tag: ''
-    }
-  }
-
-  // const counter = useSelector(state => state.isLogged)
-
   render() {
+    let NavBar;
+    if (this.props.isLoggedIn) {
+      NavBar = (
+        <React.Fragment>
+          <AdditionWrapper>
+            <div>
+              <AdditionButton href="/login">
+                <div>log out</div>
+              </AdditionButton>
+            </div>
+          </AdditionWrapper>
+        </React.Fragment>
+      );
+    } else {
+      NavBar = (
+        <React.Fragment>
+          <AdditionWrapper>
+            <div>
+              <AdditionButton href="/signup">sign up</AdditionButton>
+            </div>
+          </AdditionWrapper>
+          <AdditionWrapper>
+            <div>
+              <AdditionButton href="/login">
+                <div>log in</div>
+              </AdditionButton>
+            </div>
+          </AdditionWrapper>
+        </React.Fragment>
+      );
+    }
     return (
       <Fragment>
         <OuterDiv>
           <InnerDiv>
             <SearchDiv>
               <LogoDiv>
-                <div><LogoButton href="/userMain"><div>Logo</div></LogoButton></div>
+                <div>
+                  <LogoButton href="/userMain">
+                    <div>Logo</div>
+                  </LogoButton>
+                </div>
               </LogoDiv>
               <NavDiv>
-                <div><NavSearch></NavSearch></div>
+                <div>
+                  <NavSearch></NavSearch>
+                </div>
               </NavDiv>
             </SearchDiv>
             <AdditionDiv>
+              {NavBar}
               <AdditionWrapper>
-                <div><AdditionButton href="/signup">sign up</AdditionButton></div>
-              </AdditionWrapper>
-              <AdditionWrapper>
-                <div><AdditionButton href="/login"><div>log in</div></AdditionButton></div>
-              </AdditionWrapper>
-              <AdditionWrapper>
-                <div><AdditionButton href="/trackr"><div>trackr</div></AdditionButton></div>
+                <div>
+                  <AdditionButton href="/trackr">
+                    <div>trackr</div>
+                  </AdditionButton>
+                </div>
               </AdditionWrapper>
             </AdditionDiv>
           </InnerDiv>
         </OuterDiv>
-        <SearchTag>{this.state.tag}</SearchTag>
       </Fragment>
     );
   }
-
-  getSearchTag() {
-    if (this.state.inputValue != '') {
-      this.setState({
-        tag: "#" + this.state.inputValue,
-        inputValue: ''
-      })
-    } else {
-      this.setState({
-        tag: ''
-      })
-    }
-  }
-
-  handleInputChange(event) {
-    this.setState({
-      inputValue: event.target.value
-    })
-  }
 }
 
-export default Navigation;
+export default connect(mapStateToProps)(Navigation);
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./styles/styles.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createStore, combineReducers } from "redux";
+import { createStore } from "redux";
 import allReducers from "./reducers";
 import { Provider } from "react-redux";
 
-const store = createStore(allReducers);
-console.log(store);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
 
 ReactDOM.render(
   <Provider store={store}>
