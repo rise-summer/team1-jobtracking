1bccc19f2bc962a3bf1707820669ccfede792b28
andrew <andrew.liang@stonybrook.edu>
2020-12-23 04:53:55 -0500

    added middleware to check for firebase header token auth

--- server/server/app/model/userModel.js
+++ server/server/app/model/userModel.js
@@ -1,14 +1,12 @@
 let sql = require('../db');
 const uuid = require('uuid');
-const firebase = require('firebase/app');
-require('firebase/auth');
 
-const bcrypt = require('bcrypt');
-const saltRounds = 5;
+// const firebase = require('firebase/app');
+// require('firebase/auth');
 
-const { firebaseConfig } = require('../config');
+// const { firebaseConfig } = require('../config');
 
-const fbapp = firebase.initializeApp(firebaseConfig);
+// const fbapp = firebase.initializeApp(firebaseConfig);
 
 // user object construction
 function User(user) {
@@ -16,19 +14,17 @@ function User(user) {
   this.id = uuid.v4();
   this.username = user.username;
   this.email = user.email;
-
-  const hash = bcrypt.hashSync(user.password, saltRounds);
-  this.password = hash;
+  this.password=user.password;
 }
 
 User.createUser = (newUser, result) => {
-  fbapp
-    .auth()
-    .createUserWithEmailAndPassword(newUser.email, newUser.password)
-    .catch((err) => {
-      result(err, null);
-      return;
-    });
+  // fbapp
+  //   .auth()
+  //   .createUserWithEmailAndPassword(newUser.email, newUser.password)
+  //   .catch((err) => {
+  //     result(err, null);
+  //     return;
+  //   });
   
   let stmt=`INSERT INTO users(username,email,password)
   VALUES(?,?,?)`;
