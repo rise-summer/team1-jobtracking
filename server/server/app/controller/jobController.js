7da437ce21cbaea6d778357a67c840650d3ea0d6
andrew <andrew.liang@stonybrook.edu>
2021-01-08 03:22:09 -0500

    renamed res object to avoid error and confusion

--- server/server/app/controller/jobController.js
+++ server/server/app/controller/jobController.js
@@ -2,19 +2,22 @@ let Job = require('../model/jobModel');
 let User = require('../model/userModel');
 
 exports.create_job_app = async (req, res) => {
-    let userId=-1
-    User.getUserByEmail(req.userEmail,(err,id)=>{
-        userId=id;
+    // console.log(req.body.userEmail)
+    User.getUserByEmail(req.body.userEmail,(err,result)=>{
+        // console.log('result from getuser:'+err+' '+result)
+        userId=result[0].id;
+        // console.log(result[0])
+        const newJob = new Job(req.body,userId);
+        Job.createJob(newJob, (err, job) => {
+            if (err == null) {
+                res.status(200).send({ error: false, message: 'Job successfully created.'});
+                return;
+            }
+            else{
+                res.status(409).send({ error: true, message: err.message})
+            }
+        });
     });
-    const newJob = new Job(req.body,userId);
-
-    Job.createJob(newJob, (err, job) => {
-        if (err == null) {
-            res.status(200).send({ error: false, message: 'Job successfully created.'});
-            return;
-        }
-        else{
-            res.status(409).send({ error: true, message: 'Error while trying to create job.'})
-        }
-    });
+    
+    
 };
