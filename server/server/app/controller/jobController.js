a379250060f2f275c33e14635a6a7bb0e900b820
andrew <andrew.liang@stonybrook.edu>
2021-01-08 16:19:30 -0500

    added error checking and update_job func

--- server/server/app/controller/jobController.js
+++ server/server/app/controller/jobController.js
@@ -5,16 +5,21 @@ exports.create_job_app = async (req, res) => {
     // console.log(req.body.userEmail)
     User.getUserByEmail(req.body.userEmail,(err,result)=>{
         // console.log('result from getuser:'+err+' '+result)
+        if(err){
+            res.status(409).send({ error: true, message: err.message})
+            return;
+        }
         userId=result[0].id;
         // console.log(result[0])
         const newJob = new Job(req.body,userId);
-        Job.createJob(newJob, (err, job) => {
-            if (err == null) {
+        // console.log(newJob)
+        Job.createJob(newJob, (error, job) => {
+            if (error == null) {
                 res.status(200).send({ error: false, message: 'Job successfully created.'});
                 return;
             }
             else{
-                res.status(409).send({ error: true, message: err.message})
+                res.status(409).send({ error: true, message: error.message})
                 return;
             }
         });
@@ -24,14 +29,18 @@ exports.create_job_app = async (req, res) => {
 
 exports.get_jobs= async (req,res)=>{
     User.getUserByEmail(req.body.userEmail,(err,result)=>{
+        if(err){
+            res.status(409).send({ error: true, message: err.message})
+            return;
+        }
         userId=result[0].id;
-        Job.getAllJobsForUser(userId,(err,jobs)=>{
-            if (err == null) {
+        Job.getAllJobsForUser(userId,(error,jobs)=>{
+            if (error == null) {
                 res.status(200).send({ error: false, data: jobs});
                 return;
             }
             else{
-                res.status(409).send({ error: true, message: err.message})
+                res.status(409).send({ error: true, message: error.message})
                 return;
             }
         })
@@ -40,10 +49,14 @@ exports.get_jobs= async (req,res)=>{
 
 exports.get_job=async(req,res)=>{
     User.getUserByEmail(req.body.userEmail,(err,result)=>{
+        if(err){
+            res.status(409).send({ error: true, message: err.message})
+            return;
+        }
         userId=result[0].id;
         jobId=req.params.jobId
-        Job.getJobById(jobId,(err,job)=>{
-            if (err == null) {
+        Job.getJobById(jobId,(error,job)=>{
+            if (error == null) {
                 // console.log(job[0].user_id)
                 if (job[0].user_id==userId){
                     res.status(200).send({ error: false, data: job});
@@ -56,7 +69,7 @@ exports.get_job=async(req,res)=>{
                 
             }
             else{
-                res.status(409).send({ error: true, message: err.message})
+                res.status(409).send({ error: true, message: error.message})
                 return;
             }
         })
@@ -66,10 +79,14 @@ exports.get_job=async(req,res)=>{
 
 exports.delete_job=async(req,res)=>{
     User.getUserByEmail(req.body.userEmail,(err,result)=>{
+        if(err){
+            res.status(409).send({ error: true, message: err.message})
+            return;
+        }
         userId=result[0].id;
         jobId=req.params.jobId;
-        Job.getJobById(jobId,(err,job)=>{
-            if (err == null) {
+        Job.getJobById(jobId,(errno,job)=>{
+            if (errno == null) {
                 // console.log(job[0].user_id)
                 if (job[0].user_id==userId){
                     //allowed
@@ -80,6 +97,7 @@ exports.delete_job=async(req,res)=>{
                         }
                         else{
                             res.status(409).send({ error: true, message: error.message})
+                            return;
                         }
                     })
                 }
@@ -94,5 +112,45 @@ exports.delete_job=async(req,res)=>{
                 return;
             }
         })
+    })
+}
+
+exports.update_job=async(req,res)=>{
+    User.getUserByEmail(req.body.userEmail,(err,result)=>{
+        if(err){
+            res.status(409).send({ error: true, message: err.message})
+            return;
+        }
+        userId=result[0].id;
+        jobId=req.params.jobId;
+        Job.getJobById(jobId,(errno,job)=>{
+            if (errno == null) {
+                // console.log(job[0].user_id)
+                if (job[0].user_id==userId){
+                    //allowed
+                    const updatedJob=new Job(req.body,userId);
+                    updatedJob.job_id=jobId
+                    Job.update(updatedJob,(error,results)=>{
+                        if(error==null){
+                            res.status(200).send({ error: false, data: updatedJob});
+                            return;
+                        }
+                        else{
+                            res.status(409).send({ error: true, message: error.message})
+                            return;
+                        }
+                    })
+                }
+                else{
+                    //not allowed
+                    res.status(403).send('Unauthorized');
+                    return;
+                }
+            }
+            else{
+                res.status(409).send({ error: true, message: errno.message})
+                return;
+            }
+        })        
     })
 }
