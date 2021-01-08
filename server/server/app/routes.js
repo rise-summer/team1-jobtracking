a57b7435b624bbe159a1ab6a8ef77dba635255a2
andrew <andrew.liang@stonybrook.edu>
2021-01-08 04:33:06 -0500

    route to get a single job

--- server/server/app/routes.js
+++ server/server/app/routes.js
@@ -7,7 +7,8 @@ module.exports = function (app) {
   app.route('/api/*').all(checkAuth)
   // user routes
   app.route('/api/register').post(userController.create_a_user);
-  app.route('/api/users').get(userController.list_all_users);
+  // app.route('/api/users').get(userController.list_all_users);
   app.route('/create_job').post(jobController.create_job_app);
-  app.route('/get_jobs').get(jobController.get_job_apps_by_user);
+  app.route('/get_jobs').get(jobController.get_jobs);
+  app.route('/job/:jobId').get(jobController.get_job); // Access jobId via: req.params.jobId
 };
