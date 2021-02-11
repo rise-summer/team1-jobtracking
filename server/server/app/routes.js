09d7087194a9047e916a30ccad7800af46ce4609
RileyHYZ <ri.hyzhou@gmail.com>
2021-02-10 23:46:17 -0800

    added api to the path url

--- server/server/app/routes.js
+++ server/server/app/routes.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
   app.route('/api/job/:jobId').get(jobController.get_job); // Access jobId via: req.params.jobId
   app.route('/api/job/delete/:jobId').delete(jobController.delete_job);
   app.route('/api/job/update/:jobId').put(jobController.update_job);
-  app.route('/post/create').post(postController.create_new_post);
-  app.route('/post/getUserPosts/:userId').get(postController.get_posts_of_user);
-  app.route('/post/fetchPosts').get(postController.get_posts);
+  app.route('/api/post/create').post(postController.create_new_post);
+  app.route('/api/post/getUserPosts/:userId').get(postController.get_posts_of_user);
+  app.route('/api/post/fetchPosts').get(postController.get_posts);
 };
