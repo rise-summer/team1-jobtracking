1b1b2127ec39be40436a2d21258a3e8648d67749
RileyHYZ <ri.hyzhou@gmail.com>
2021-02-10 23:53:46 -0800

    updated adding post and showing posts

--- src/components/mainfeed/index.js
+++ src/components/mainfeed/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext } from "react";
 import Navigation from "../navigation";
 import { Heading, Text, NewPostButton, BackgroundDiv, MainBody } from "./style";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AddPost from "./AddPost";
 import Post from "./Post";
 import axios from 'axios';
@@ -14,27 +14,18 @@ const MainFeed = () => {
   const [authentication, setAuthentication] = useContext(AuthenticationContext);
 
   const [numPosts, setNumPosts] = useState(1);
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      author: 'John',
-      title: 'Hive Summer 2021 Internship Positions Now Available!',
-      date: '01/03/21',
-      description: "They have positions for electrical engineering, business, and computer science majors. The deadline to apply is 1/17/21.",
-    },
-  ]);
-  // let posts = [];
-  // useEffect(async () => {
-  //   const result = await axios.get(`/post/getPosts`);
-  //   setPosts([...result.posts]);
-  // });
+  const [posts, setPosts] = useState([]);
+  useEffect(() => { 
+    async function fetchPosts() {
+      const result = await axios.get(`http://localhost:5000/api/post/fetchPosts`);
+      console.log(result.request);
+      setPosts(result.data.body);
+    }
+    fetchPosts();
+  }, []);
 
   if (!authentication.displayName) {
     return (<Redirect to="/login" />);
-  }
-
-  const addPost = async() => {
-
   }
 
   return (
@@ -48,7 +39,7 @@ const MainFeed = () => {
             {/* <NewPostButton>Create New Post</NewPostButton> */}
           </Heading>
           {posts.map((post) => (
-            <Post id={post.id} author={post.author} title={post.title} date={post.date} description={post.description} />
+            <Post id={post.id} author={post.author} title={post.title} date={post.date} description={post.content} />
           ))}
         </BackgroundDiv>
       </MainBody>
