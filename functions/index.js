625cb9780dabd2666005962e5cb21c686056bda2
nxgrizzle <nxgrizzle@gmail.com>
2022-07-29 16:22:25 -0400

    fixed indeed route

--- functions/index.js
+++ functions/index.js
@@ -114,8 +114,6 @@ exports.indeedScraper = functions
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"
     );
     await page.goto(req.link);
-    const html = await page.content();
-    console.log(html);
     await page.waitForSelector(".jobsearch-JobInfoHeader-title");
     const result = await page.evaluate(() => {
       let title = document.querySelector(
